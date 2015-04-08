030423c7e6a53810faecdb9451d49eca28551594
Vitaliy <elanceShevchenko@gmail.com>
2015-04-08 06:26:40 +0300

    added connection to mongolab db and couple routes

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-// server.js
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
+var Drugs     = require('./app/models/drugs');
+
 mongoose.connect('mongodb://drugs:drugs@ds039251.mongolab.com:39251/drugs'); // connect to our database
-var Bear     = require('./app/models/bear');
+
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // middleware to use for all requests
@@ -33,82 +27,47 @@ router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' });
 });
 
-router.route('/bears')
-
-  // create a bear (accessed at POST http://localhost:8080/api/bears)
-  .post(function(req, res) {
-
-    var bear = new Bear();      // create a new instance of the Bear model
-    bear.name = req.body.name;  // set the bears name (comes from the request)
-
-    bear.save(function(err) {
-      if (err)
-        res.send(err);
-
-      res.json({ message: 'Bear created!' });
-    });
-
-  })
+router.route('/drugs')
   .get(function(req, res) {
-    Bear.find(function(err, bears) {
-      if (err)
+    Drugs.find(function(err, drugs) {
+      if (err){
         res.send(err);
-
-      res.json(bears);
+      }
+      res.json(drugs);
     });
   });
 
-router.route('/bears/:bear_id')
+router.route('/drugs/:drug_id')
 
-  // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
   .get(function(req, res) {
-    Bear.findById(req.params.bear_id, function(err, bear) {
-      if (err)
+    Drugs.findById(req.params.drug_id, function(err, drug) {
+      if (err){
         res.send(err);
-      res.json(bear);
+      }
+      res.json(drug);
     });
   })
 
-  // update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
   .put(function(req, res) {
 
-    // use our bear model to find the bear we want
-    Bear.findById(req.params.bear_id, function(err, bear) {
-
-      if (err)
+    Drugs.findById(req.params.drug_id, function(err, drug) {
+      if (err){
         res.send(err);
+      }
+      drug.name = req.body.name;  // update name
 
-      bear.name = req.body.name;  // update the bears info
-
-      // save the bear
-      bear.save(function(err) {
-        if (err)
+      // save the drug
+      drug.save(function(err) {
+        if (err){
           res.send(err);
-
-        res.json({ message: 'Bear updated!' });
+        }
+        res.json({ message: 'object updated' });
       });
-
-    });
-  })
-
-  // delete the bear with this id (accessed at DELETE http://localhost:8080/api/bears/:bear_id)
-  .delete(function(req, res) {
-    Bear.remove({
-      _id: req.params.bear_id
-    }, function(err, bear) {
-      if (err)
-        res.send(err);
-
-      res.json({ message: 'Successfully deleted' });
     });
   });
 
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
+
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('listening port:' + port);
